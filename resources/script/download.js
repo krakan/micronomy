17071e79da6c32d213363b7e8dba921a92d6ec85
Oscar Haglund <Zaladar@github.com>
2022-03-18 15:04:32 +0100

    Java script for downloading ics file to machine/ cellphone should be agnostic in the sense that it alsow should work on the cell phone but it is not properly tested.

---
+++ resources/script/download.js
@@ -0,0 +1,11 @@
+var icsElement = document.getElementById("ics");
+if (icsElement.innerText != ""){ 
+    var a = document.createElement('a');
+    var yrinput = document.getElementById("yearinput");
+    yrinput.append(a);
+    a.setAttribute('download','Calendar.ics')
+    a.innerText = "Hello world!";
+    a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(icsElement.innerText));
+    a.setAttribute('class', 'save-button';
+}
+
