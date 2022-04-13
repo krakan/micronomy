72d31dde97964fdfcbfdcc78ed7b12e51086fde8
Oscar Haglund <Zaladar@github.com>
2022-04-13 17:09:30 +0200

    missing parenthesis in javascript.

--- resources/script/download.js
+++ resources/script/download.js
@@ -3,9 +3,9 @@ if (icsElement.innerText != ""){
     var a = document.createElement('a');
     var yrinput = document.getElementById("yearinput");
     yrinput.append(a);
-    a.setAttribute('download','Calendar.ics')
+    a.setAttribute('download','Calendar.ics');
     a.innerText = "Hello world!";
     a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(icsElement.innerText));
-    a.setAttribute('class', 'save-button';
+    a.setAttribute('class', 'save-button');
 }
 
