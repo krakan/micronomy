811fffb2f4987daa69a9338bd66e0ed4f510ddf4
Jonas Linde <jonasjlinde@gmail.com>
2025-12-27 23:28:27 +0100

    restart the micronomy service after the wait-for-restart service

--- memfree.sh
+++ memfree.sh
@@ -3,6 +3,15 @@
 while memf=$(grep MemF /proc/meminfo | tr -s ' ' '\t' | cut -f2)
 do
     date "+%FT%T $memf"
-    test $memf -lt 100000 && pkill raku
+    if test $memf -lt 100000
+    then
+        sudo systemctl restart wait-for-restart
+        for i in $(seq 10)
+        do
+            netstat -ln | grep ':8888 .* LISTEN' && break
+            sleep 0.5
+        done
+        pkill -fx 'raku -I lib service.raku'
+    fi
     sleep 60
 done
--- micronomy.sh
+++ micronomy.sh
@@ -76,6 +76,11 @@ case $target in
 
     tmux) ssh -t micronomy.jonaseel.se tmux attach;;
 
+    wait-for-restart)
+        mkdir -p $scriptdir/log
+        script -c "raku wait-for-restart.raku" -f $scriptdir/log/restarter-$(date +%Y%m%d%H%M%S).log 2>&1
+        ;;
+
     run)
         # run as root to allow low port
         if test ${port:-0} -lt 1024
@@ -151,7 +156,6 @@ case $target in
                 script -c "raku -I lib service.raku" -f /var/log/micronomy-$(date +%Y%m%d%H%M%S).log 2>&1
             else
                 mkdir -p $scriptdir/log
-                script -c "raku -I lib wait-for-restart.raku" -f $scriptdir/log/restarter-$(date +%Y%m%d%H%M%S).log 2>&1 &
                 script -c "raku -I lib service.raku" -f $scriptdir/log/micronomy-$(date +%Y%m%d%H%M%S).log 2>&1
             fi
             test $TMUX && tmux set automatic-rename
