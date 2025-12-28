8847aff697a04bb9717d5d8f1621fe809ab1c07b
Jonas Linde <jonasjlinde@gmail.com>
2025-12-28 13:17:04 +0100

    attempt requesting garbage collection before kill raku to force it

--- memfree.sh
+++ memfree.sh
@@ -5,16 +5,13 @@ do
     date "+%FT%T $memf"
     if test $memf -lt 100000
     then
-        # Restart the wait-for-restart service
-        sudo systemctl restart wait-for-restart
-        for i in $(seq 10)
-        do
-            sudo netstat -pln | grep ':8888 .* LISTEN' && break
-            sleep 0.5
-        done
+        # Try garbage collection
+        raku -e 'VM.request-garbage-collection'
+        sleep 1
 
-        # Kill the micronomy process to let it restart
-        pkill -fx 'raku -I lib service.raku'
+        # Kill all raku processes to force garbage collection
+        memf=$(grep MemF /proc/meminfo | tr -s ' ' '\t' | cut -f2)
+        test $memf -lt 100000 && pkill raku
     fi
     sleep 60
 done
